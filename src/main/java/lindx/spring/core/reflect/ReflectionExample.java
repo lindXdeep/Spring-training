037d8127a7a927d4233d3050de8269d5817bc72b
lindx <linder.igor@gmail.com>
2021-01-31 08:06:50 +0300

    reflection api

--- src/main/java/lindx/spring/core/reflect/ReflectionExample.java
+++ src/main/java/lindx/spring/core/reflect/ReflectionExample.java
@@ -9,5 +9,18 @@ public class ReflectionExample {
         for (Method method : Arrays.class.getMethods()){
             method.getName().lines().forEach(System.out::println);
         }
+        
+        Runnable r = new Runnable(){
+            @Override
+            public void run() {
+                System.out.println("hello");
+            }
+        };
+
+        System.out.println(Runnable.class.getName());
+
+        System.out.println(r.getClass().getName());
+
+
     }
 }
