9b6e29a3c5013a7c9940c4f0b20967bace3931db
lindx <linder.igor@gmail.com>
2021-01-31 17:07:17 +0300

    cd /home/lindx/ProjectsPreproject/Spring-the-training ; /home/lindx/dev-libs/java/jdk-14.0.1/bin/java -XX:+ShowCodeDetailsInExceptionMessages -Dfile.encoding=UTF-8 @/tmp/cp_bsa4sup7w7admq13ma7qqy8m.argfile lindx.spring.core.reflect.ReflectionExample

--- src/main/java/lindx/spring/core/reflect/ReflectionExample.java
+++ src/main/java/lindx/spring/core/reflect/ReflectionExample.java
@@ -1,15 +1,20 @@
 package lindx.spring.core.reflect;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+
 public class ReflectionExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
         
+        // Масисв методов
         for (Method method : Arrays.class.getMethods()){
             method.getName().lines().forEach(System.out::println);
         }
-        
+
+        // У анонимного класса есть имя
         Runnable r = new Runnable(){
             @Override
             public void run() {
@@ -17,10 +22,11 @@ public class ReflectionExample {
             }
         };
 
-        System.out.println(Runnable.class.getName());
+        System.out.println(r.getClass().getName()); // lindx.spring.core.reflect.ReflectionExample$1
 
-        System.out.println(r.getClass().getName());
+        // Создаем обьект с помощью механизма рефлексии
+        Runnable r2 = r.getClass().newInstance();
 
-
+        r2.run();                                   // hello
     }
 }
