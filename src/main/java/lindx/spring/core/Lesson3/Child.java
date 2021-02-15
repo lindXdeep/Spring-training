bca7306372e5d1f7384d276b624573d1103d20b2
lindx <linder.igor@gmail.com>
2021-02-15 04:29:34 +0300

    Dependency injection - circle-dependency

---
+++ src/main/java/lindx/spring/core/Lesson3/Child.java
@@ -0,0 +1,43 @@
+package lindx.spring.core.Lesson3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import lindx.spring.core.reflect.context.Component;
+
+@Component
+public class Child {
+
+    private String name;
+    private Parent parent;
+
+    public Child( @Value(value = "Billy") String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parent getParent() {
+        return this.parent;
+    }
+
+    @Autowired
+    public void setParent(Parent parent) {
+        System.out.println(parent.getChild() == null);
+        this.parent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " name='" + getName() + "'" +
+            ", parent='" + getParent().getName() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/lindx/spring/core/Lesson3/Parent.java
@@ -0,0 +1,43 @@
+package lindx.spring.core.Lesson3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Parent {
+    private String name;
+    private Child child;
+
+    public Parent(@Value(value = "batya") String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Child getChild() {
+        return this.child;
+    }
+    @Autowired
+    public void setChild(Child child) {
+        System.out.println(child.getParent() == null);
+        this.child = child;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " name='" + getName() + "'" +
+            ", child='" + getChild().getName() + "'" +
+            "}";
+    }
+
+}
+
